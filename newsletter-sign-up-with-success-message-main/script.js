c86b78f209da3ef81a04122a698234dd431519be
Kayk Edmar <kayksantana133@gmail.com>
2023-12-22 18:01:44 -0300

    att

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,4 +1,9 @@
-const entrada = document.querySelector('.entrada-js')
+const entrada = document.querySelector('.entrada-usuario')
 const btnEnvio = document.querySelector('.envioBtn')
+const success = document.querySelector('.success')
+const main = document.querySelector('main')
 
-
+btnEnvio.addEventListener('click', () => {
+  main.classList.add('hide')
+  success.classList.remove('hide')
+})
