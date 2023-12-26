225a60c283f02ccaafbdff1af5857760685d0a70
Kayk Edmar <kayksantana133@gmail.com>
2023-12-26 20:06:07 -0300

    att

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,9 +1,28 @@
-const entrada = document.querySelector('.entrada-usuario')
-const btnEnvio = document.querySelector('.envioBtn')
-const success = document.querySelector('.success')
-const main = document.querySelector('main')
+const userInput = document.querySelector(".entrada-user");
+const btnEnvio = document.querySelector(".envioBtn");
+const btnBack = document.querySelector(".btnBack");
+const success = document.querySelector(".success");
+const main = document.querySelector("main");
+const errorMessage = document.querySelector(".labels .error-email");
 
-btnEnvio.addEventListener('click', () => {
-  main.classList.add('hide')
-  success.classList.remove('hide')
-})
+btnEnvio.addEventListener("click", () => {
+  inputUser = userInput.value;
+
+  // Adiciona a classe 'hide' à mensagem de erro ao clicar no botão
+  errorMessage.classList.add("hide");
+
+  if (inputUser === "" || !inputUser.includes("@")) {
+    errorMessage.classList.remove("hide");
+    console.log("Digite um e-mail válido");
+  } else {
+    success.classList.remove("hide");
+    main.classList.add("hide");
+  }
+});
+
+btnBack.addEventListener("click", () => {
+  success.classList.add("hide");
+  main.classList.remove("hide");
+
+  userInput.value = "";
+});
